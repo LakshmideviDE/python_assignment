d5a2d600b5776ea68b0684488ddaef9dc1121174
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-22 19:20:52 +0530

    mutated string character

--- src/assignment3/util.py
+++ src/assignment3/util.py
@@ -1,7 +1,7 @@
-def mutate_string():
-    input_string = input("Enter a string: ")
-    position = int(input("Enter the position to mutate: "))
-    character = input("Enter the character to replace: ")
+def mut_st():
+        input_string = input("Enter a string: ")
+        position = int(input("Enter the position to mutate: "))
+        character = input("Enter the character to replace: ")
 
-    result = input_string[:position] + character + input_string[position + 1:]
-    return result
+        result = input_string[:position] + character + input_string[position + 1:]
+        return result
